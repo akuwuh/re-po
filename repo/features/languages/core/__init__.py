45183d3e2c0edd8808d439842ad8d8f77a2f3ad4
akuwuh <isaacnguyen03@gmail.com>
2026-02-05 21:46:37 -0500

    refactor(languages): export request contract from core package

--- repo/features/languages/core/__init__.py
+++ repo/features/languages/core/__init__.py
@@ -1,4 +1,5 @@
 from .config import RenderConfig, ThemeColors
+from .request import LanguagesRequest
 from .service import LanguageStatsService
 
-__all__ = ['RenderConfig', 'ThemeColors', 'LanguageStatsService']
+__all__ = ['RenderConfig', 'ThemeColors', 'LanguagesRequest', 'LanguageStatsService']
